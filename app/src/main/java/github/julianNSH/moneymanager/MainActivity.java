4e8242ef970e05ad39816411748d1e92b2289f2b
JulianNSH@users.noreply.github.com <iulianpanis@gmail.com>
2021-07-12 16:27:47 +0300

    SplashScreen/FAB

--- app/src/main/java/github/julianNSH/moneymanager/MainActivity.java
+++ app/src/main/java/github/julianNSH/moneymanager/MainActivity.java
@@ -1,8 +1,11 @@
 package github.julianNSH.moneymanager;
 
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -10,22 +13,35 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import github.julianNSH.moneymanager.add.AddFragment;
+
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(R.style.Theme_MoneyManager);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.floating_add);
+        fab.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment,
+                        new AddFragment()).commit();
+            }
+        });
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_overview, R.id.navigation_statistics, R.id.navigation_scope,
-                R.id.navigation_savings, R.id.navigation_add)
+                R.id.navigation_savings, R.id.floating_add)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
     }
 
 }
